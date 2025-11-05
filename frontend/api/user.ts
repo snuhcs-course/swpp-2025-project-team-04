80c2b46b7382b41755026109e82687df9e9f528a
Junghoon Kim <jafarock@naver.com>
2025-11-05 22:52:03 +0900

    :wrench: fix: 404 error caused by replacing getMe function

--- frontend/api/user.ts
+++ frontend/api/user.ts
@@ -1,6 +1,12 @@
 import { User } from '@/types/type';
 import { customFetch } from './client';
 
+export const getMe = async (): Promise<User> => {
+  return customFetch<User>('/users/me', {
+    method: 'GET',
+  });
+};
+
 export const getUserById = async (userId: number): Promise<User> => {
   return customFetch<User>(`/users/${userId}`, {
     method: 'GET',
